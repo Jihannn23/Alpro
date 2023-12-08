1e2c49a8d64824a03f1a06315c3ef290be2f24a2
Jihannn23 <149360567+Jihannn23@users.noreply.github.com>
2023-12-09 04:54:31 +0700

    Update Project algo.cpp

--- Project algo.cpp
+++ Project algo.cpp
@@ -1,32 +1,27 @@
-void daftarTamu() {
+void checkout() {
     system("CLS");
     header();
+    gotoxy(40, 8);
+    cout << "---------- CHECKOUT ----------";
 
-    int row = 9;
+    ifstream inFile("tamu123.txt");
+    ifstream kamarfile("kamar.txt");
+    ofstream kamaroutfile("temp1.txt");
+    ofstream outFile("temp.txt");
 
-    gotoxy(40, 7);
-    cout << "--------- DAFTAR TAMU ---------" << endl << endl;
-
-    ifstream infile("tamu123.txt");
-
-    if (!infile.is_open()) {
-        cout << "File not found." << endl;
+    if (!inFile.is_open() || !outFile.is_open() 
+        || !kamarfile.is_open() || !kamaroutfile.is_open() ) {
+        cout << "Error opening files." << endl;
         return;
     }
 
-    string noKamar;
-    while (infile >> noKamar) {
+    string roomNumber;
+    gotoxy(40, 9);
+    cout << "Masukkan No. Kamar  :";
+    cin >> roomNumber;
 
-        string line;
-        for (int i = 0; i < 5; ++i) {
-            getline(infile, line);
-            gotoxy(42, row++);
-            cout << line << endl;
-        }
-
-        gotoxy(30, row++);
-        cout << setfill('=') << setw(50) << "" << endl;
-    }
-
-    infile.close();
-}
+    string line;
+    string line2;
+    bool found1 = false;
+    bool found2 = false;
+    int row = 11;
