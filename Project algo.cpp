5f3fe06865e7310c9d20293231e420e13d9dffc5
Jihannn23 <149360567+Jihannn23@users.noreply.github.com>
2023-12-09 05:05:48 +0700

    Update Project algo.cpp

--- Project algo.cpp
+++ Project algo.cpp
@@ -1,27 +1,31 @@
-void checkout() {
+void daftarTamu() {
     system("CLS");
     header();
-    gotoxy(40, 8);
-    cout << "---------- CHECKOUT ----------";
+    int row = 9;
 
-    ifstream inFile("tamu123.txt");
-    ifstream kamarfile("kamar.txt");
-    ofstream kamaroutfile("temp1.txt");
-    ofstream outFile("temp.txt");
+    gotoxy(40, 7);
+    cout << "--------- DAFTAR TAMU ---------" << endl << endl;
 
-    if (!inFile.is_open() || !outFile.is_open() 
-        || !kamarfile.is_open() || !kamaroutfile.is_open() ) {
-        cout << "Error opening files." << endl;
+    ifstream infile("tamu123.txt");
+
+    if (!infile.is_open()) {
+        cout << "File not found." << endl;
         return;
     }
 
-    string roomNumber;
-    gotoxy(40, 9);
-    cout << "Masukkan No. Kamar  :";
-    cin >> roomNumber;
+    string noKamar;
+    while (infile >> noKamar) {
 
-    string line;
-    string line2;
-    bool found1 = false;
-    bool found2 = false;
-    int row = 11;
+        string line;
+        for (int i = 0; i < 5; ++i) {
+            getline(infile, line);
+            gotoxy(42, row++);
+            cout << line << endl;
+        }
+
+        gotoxy(30, row++);
+        cout << setfill('=') << setw(50) << "" << endl;
+    }
+
+    infile.close();
+}
